3c402e5cb6ba608dc27c44de62236d8790bf8344
sookiemonster <sookiemonster216@gmail.com>
2025-01-10 16:39:21 -0500

    Add basic Mantine theme for colors & fonts. Add font imports to index.html.
    Add example code for coloring title & components. Add

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,9 +5,11 @@ import { MantineProvider } from '@mantine/core'
 import '@mantine/core/styles.css'
 import Router from '@/Router'
 
+import { KissenaTheme } from '@/kissena/theme'
+
 function App() {
   return (
-    <MantineProvider>
+    <MantineProvider theme={KissenaTheme}>
       <Router />
     </MantineProvider>
   )
