4bff8cf38f4373254d12d0ee5019772fc32c42df
Proma Augustina Rozario <augustinarozario2004@gmail.com>
2025-01-15 10:10:17 -0500

    Changes in Home.page.tsx

--- frontend/src/kissena/pages/home/Home.page.tsx
+++ frontend/src/kissena/pages/home/Home.page.tsx
@@ -1,9 +1,21 @@
-import { Outlet } from 'react-router-dom'
+//import { Outlet } from 'react-router-dom'
+
+//export function HomePage() {
+ // return (
+   // <>
+   //   Home! <Outlet />
+   // </>
+ // )
+//}
+
+import { Outlet } from 'react-router-dom';
 
 export function HomePage() {
   return (
-    <>
-      Home! <Outlet />
-    </>
-  )
+    <div>
+      <h1>Home Page</h1>
+      <Outlet /> {/* This will render any nested routes defined in the 'Home' route */}
+    </div>
+  );
 }
+
