3cd304e6331295b61e78831e799af945d87899ad
Daniel Sooknanan <daniel.sooknanan216@gmail.com>
2025-01-18 14:35:41 -0500

    Add custom styles to Pagination Control. Add fixed positioning for Pagination Control. Add basic logic for paginated content rendering.

--- frontend/src/kissena/components/Pagination/PaginationContext.tsx
+++ frontend/src/kissena/components/Pagination/PaginationContext.tsx
@@ -1,8 +1,8 @@
 import { createContext } from "react";
 
 export const PaginationContext = createContext({
+    ENTRIES_PER_PAGE: 0,
     activePage: 1,
-    setPage: (n:number) => { console.log(n); return; },
     onFirstPage: true,
     onLastPage: true,
 
