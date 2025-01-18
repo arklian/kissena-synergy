1a397b783f868e45d71b09abb5d622035d59f3f6
Daniel Sooknanan <daniel.sooknanan216@gmail.com>
2025-01-17 22:50:52 -0500

    Add basic Announcement Component (no icon). Add Announcements Page skeleton & placeholder data. Add new /announcements route.
    Fix issue where values of Mantine resolver variables (eg. mantine-navbar-height) were not passed.
    
    Update Mantine theming to include an intermediary color between the light & base color.
    
    Update PageContainer padding to be responsive & shrink max-width.

---
+++ frontend/src/kissena/components/Announcement/Announcement.tsx
@@ -0,0 +1,45 @@
+import styles from'@kissena/components/Announcement/Announcement.module.css'
+import { Box, Button, Space, Stack, Text } from '@mantine/core'
+import { useMemo } from 'react'
+
+export interface AnnouncementData {
+    title:string,
+    datePosted:Date
+    description?:string
+    redirectUrl?:string
+};
+
+export function Announcement({title, datePosted, description, redirectUrl}:AnnouncementData) { 
+    const stringifiedDate = useMemo(() => {
+        return datePosted.toLocaleDateString('en-US')
+    }, [datePosted])
+
+    return (
+        <Stack gap={0} pt={'lg'} pb={'xl'} className={styles.announcementContainer}>
+            <Text size='md' c="neonGreen.6" fw={700} lh={'1.2rem'} lineClamp={2}>{title}</Text>
+            <Text size='md' c="neonGreen.9">{stringifiedDate}</Text>
+
+            
+            {
+                !description
+                ? <></>
+                : <>
+                <Space h='xs'/>
+                <Box className={styles.descriptionContainer}>
+                    <Text size='md' c="lightYellow.1">{description}</Text>
+                </Box>
+                </>
+            }
+
+            {
+                !redirectUrl
+                ? <></>
+                : <>
+                <Space h='lg'/>
+                <Button classNames={{label: styles.buttonLabel}} size='sm' w={120} color='darkGreen.4'>Learn More</Button>
+                </>
+            }
+
+        </Stack>
+    )
+}
