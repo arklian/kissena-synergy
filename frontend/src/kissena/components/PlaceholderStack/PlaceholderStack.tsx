9155b7b820e56e67051af189db26b9cf1eeb44ae
Daniel Sooknanan <daniel.sooknanan216@gmail.com>
2025-01-21 20:22:15 -0500

    Fix error where Event Carousel had too many placeholders

--- frontend/src/kissena/components/PlaceholderStack/PlaceholderStack.tsx
+++ frontend/src/kissena/components/PlaceholderStack/PlaceholderStack.tsx
@@ -1,9 +1,14 @@
 import { Stack, Skeleton } from '@mantine/core'
 
-export function PlaceholderStack({ hidden }: { hidden: boolean }) {
+interface PlaceholderStackProps {
+  hidden:boolean,
+  entries?:number
+}
+
+export function PlaceholderStack({ hidden, entries }: PlaceholderStackProps) {
   return (
     <Stack>
-      {Array.from(Array(5)).map((elem, index) => {
+      {Array.from(Array(entries ?? 5)).map((elem, index) => {
         return <Skeleton hidden={hidden} key={index} h={200} opacity={0.1} />
       })}
     </Stack>
