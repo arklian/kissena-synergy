545f54b6768bd1fc1e34dd39c77940b8ea9a3211
Henry Chen <hjc77@cornell.edu>
2025-02-11 14:38:53 -0500

    Kissena Home: Added Facebook and LinkTree
    Added some cards for Facebook and LinkTree so that they can do their
    updates there.

--- frontend/src/kissena/pages/home/Home.page.tsx
+++ frontend/src/kissena/pages/home/Home.page.tsx
@@ -1,25 +1,39 @@
 import styles from '@kissena/pages/home/Home.module.css'
 import Swiggle from '@kissena/pages/home/assets/swiggle.svg'
 import { Footer } from '@kissena/pages/home/Footer/Footer.tsx'
-import { Stack, Image, Title, Group, Text, Divider } from '@mantine/core'
+import {
+  Stack,
+  Image,
+  Title,
+  Group,
+  Text,
+  Divider,
+  Anchor,
+  ThemeIcon,
+} from '@mantine/core'
 import { motion } from 'motion/react'
+import { PageContainer } from '@kissena/components/PageContainer/PageContainer.tsx'
+import kissena_facebook from '@kissena/pages/home/assets/kissena_facebook.jpg'
+import kissena_linktree from '@kissena/pages/home/assets/kissena_linktree.jpeg'
+import { TbBrandLinktree } from 'react-icons/tb'
+import { FaFacebookSquare } from 'react-icons/fa'
 
 // import { AnnouncementBanner } from '@kissena/pages/home/AnnouncementBanner/Announcement.banner'
-import { EventCarousel } from './EventCarousel/Event.carousel'
+// import { EventCarousel } from './EventCarousel/Event.carousel'
 
 // Create a motion-supported variant of Mantine components
 const MotionTitle = motion.create(Title)
 // Horizontal offset for the hero-text from left viewport edge
 export const HEROTEXT_OFFSET_X = 100
 const landingBrief =
-  'We work together to restore the Kissena Velodrome and surrounding parklands, creating a vibrant community hub for all. Join us in making a positive impact on our neighborhood.'
+  'Working together to restore the Kissena Velodrome and surrounding parklands, creating a vibrant community hub for all. Join us in making a positive impact on our neighborhood.'
 
 export function HomePage() {
   return (
     <>
-      <Stack gap={0}>
-        {/*<AnnouncementBanner />*/}
-        <Stack h={'100%'} gap={'30'}>
+      {/*<AnnouncementBanner />*/}
+      <Stack h={'100%'} align={'center'}>
+        <Stack align={'center'} maw={'1800px'} w={'100%'}>
           <div className={styles.splashContainer}>
             <Stack className={styles.heroTextContainer} mt={'auto'}>
               <MotionTitle
@@ -66,25 +80,100 @@ export function HomePage() {
             </Group>
           </div>
         </Stack>
+      </Stack>
+      <PageContainer>
         <Text
-          pl={HEROTEXT_OFFSET_X}
-          pr={HEROTEXT_OFFSET_X}
-          pt={'1rem'}
+          px={'24px'}
           className={styles.description}
-          size="lg"
+          c={'lightYellow.1'}
+          size="xl"
           ta="left"
         >
           {landingBrief}
         </Text>
-        {/*<Divider mt={'xl'} color={'neonGreen.6'} />*/}
+        <Divider mt={'xl'} mx={0} color={'neonGreen.6'} />
         {/*<Stack*/}
         {/*  align="center"*/}
         {/*  px={{ xs: 0, sm: HEROTEXT_OFFSET_X / 2, md: HEROTEXT_OFFSET_X }}*/}
         {/*>*/}
         {/*  <EventCarousel />*/}
         {/*</Stack>*/}
-      </Stack>
+        <LinksSection />
+      </PageContainer>
       <Footer />
     </>
+  )
+}
+
+function LinksSection() {
+  return (
+    <Stack align={'center'} pt={'24px'}>
+      <LinkCard
+        link={'https://www.facebook.com/groups/610957329304796'}
+        text={
+          'Find out more about current happenings in Kissena Synergy at our Facebook Group!'
+        }
+        image={kissena_facebook}
+        icon={<FaFacebookSquare />}
+        iconColor={'#4267B2'}
+      />
+      <LinkCard
+        link={'https://linktr.ee/kissenasynergy'}
+        text={'More resources from our partners on our Linktree!'}
+        image={kissena_linktree}
+        icon={<TbBrandLinktree />}
+        iconColor={'#5af25d'}
+      />
+    </Stack>
+  )
+}
+
+function LinkCard({
+  link,
+  text,
+  image,
+  icon,
+  iconColor,
+}: {
+  link: string
+  text: string
+  image: string
+  icon: any
+  iconColor: string
+}) {
+  return (
+    <Anchor href={link} w={'100%'} maw={'1000px'}>
+      <Group
+        className={styles.linkCard}
+        justify={'space-around'}
+        align={'center'}
+        bd={'2px solid darkGreen.4'}
+        bg={'darkGreen.5'}
+        py={'24px'}
+        px={'24px'}
+      >
+        <Group
+          align={'flex-start'}
+          justify={'flex-start'}
+          flex={1}
+          miw={'50%'}
+          // maw={'70%'}
+        >
+          <Text size={'xl'} fw={700} c={'lightYellow.1'}>
+            <ThemeIcon
+              color={iconColor}
+              autoContrast={true}
+              // c={'darkGreen.7'}
+              size={'lg'}
+              mr={'12px'}
+            >
+              {icon}
+            </ThemeIcon>
+            {text}
+          </Text>
+        </Group>
+        <Image h={'200px'} w={'200px'} radius={'md'} src={image}></Image>
+      </Group>
+    </Anchor>
   )
 }
